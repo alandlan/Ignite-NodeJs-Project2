8657a9f5761a8e78a4e896427f058093e841f7dd
Alan Martins <alanhorcrux@gmail.com>
2021-03-17 20:24:42 -0300

    Verificando se categoria já existe e bloqueando insert

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,14 @@ const categoriesCategoriesRepositories = new CategoriesRepositories();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesCategoriesRepositories.findByName(
+    name
+  );
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists!" });
+  }
+
   categoriesCategoriesRepositories.create({ name, description });
 
   return response.status(201).send();
